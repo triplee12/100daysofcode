4c6eb0b7a2fa25115fcb97dd76dd1fa08f77a0d6
triplee12 <tripleeoliver@gmail.com>
2022-12-15 17:29:57 +0100

    Day 3: even or odd number exercise

---
+++ day_3/README.md
@@ -0,0 +1,3 @@
+# Day 3 Of 100 Days Of Code
+
+## Conditional Flow Control - If Else
---
+++ day_3/odd_or_even.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#!/bin/env/python3
+
+"""Odd or even number"""
+number = int(input("Which number do you want to check? "))
+
+if number % 2 == 0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
