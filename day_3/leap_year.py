185e1c51e35b37cd2418cd8351cc1ebe2c7d7f0a
triplee12 <tripleeoliver@gmail.com>
2022-12-15 20:32:39 +0100

    Check if year is a leap year

---
+++ day_3/leap_year.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#!/bin/env/python3
+
+"""Leap year"""
+year = int(input("Enter year to check leap year: "))
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"{year} is a leap year.")
+        else:
+            print(f"{year} is not a leap year.")
+    else:
+        print(f"{year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")
