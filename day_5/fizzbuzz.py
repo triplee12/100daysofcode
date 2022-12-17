9ff16f207a24c805ff7df42b2811632adaa60146
triplee12 <tripleeoliver@gmail.com>
2022-12-17 15:04:05 +0100

    Fix code

--- day_5/fizzbuzz.py
+++ day_5/fizzbuzz.py
@@ -2,9 +2,11 @@
 #!/bin/env/python3
 """Fizzbuzz game"""
 for num in range(1, 101):
-    if num % 3 == 0 and not num % 5 == 0:
-        print("Fizz")
-    elif num % 5 == 0 and not num % 3 == 0:
-        print("Buzz")
-    elif num % 3 == 0 and num % 5 == 0:
+    if num % 3 == 0 and num % 5 == 0:
         print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
