a92bc896d32c9dab5430f7f314094eeddc93d2cc
triplee12 <tripleeoliver@gmail.com>
2022-12-20 20:44:23 +0100

    Paint area calculator

---
+++ day_8/README.md
@@ -0,0 +1,5 @@
+# Day 8 Of 100 Days Of Code
+
+# Function With Parameter
+
+- Paint area calculator
---
+++ day_8/paint_area.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Paint area calculator
+"""Calculate the paint area"""
+def paint_calc(height, width, cover):
+    result = (height * width) / cover
+    print(f"You'll need {round(result)} of paint.")
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
