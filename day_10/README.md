6ebac521ee901c29b9f3cbed6734e2811ed9726f
triplee12 <tripleeoliver@gmail.com>
2022-12-23 07:12:03 +0100

    Print total days in a month

---
+++ day_10/README.md
@@ -0,0 +1,5 @@
+# Day 10 Of 100 Days Of Code With Python
+
+## Function Output And Multiple Return
+
+- Days in month
---
+++ day_10/days_in_month.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Days in month program"""
+
+def is_leap(year):
+    """Check if year entered is a leap year"""
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+def days_in_month(year, month):
+    """Find how many days in a month"""
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year) == True and month_days[1] == month_days[month - 1]:
+        day = month_days[month - 1]
+        return day + 1
+    else:
+        return month_days[month - 1]
+
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
