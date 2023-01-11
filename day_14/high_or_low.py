08602775324bad0050b634bac4c8f060eaee6c7b
triplee12 <tripleeoliver@gmail.com>
2023-01-11 20:37:48 +0100

    Final commit on high or low

--- day_14/high_or_low.py
+++ day_14/high_or_low.py
@@ -1,30 +1,47 @@
 #!/usr/bin/python3
-"""high_or_low module"""
 
+import random
 
-logo = '''high or low'''
-print(logo)
+from game_data import data
 
-high = {"me":200000, "ebuka":500000, "emmanuel":1000000}
-low = {"you":100000, "emeka":400000, "colins":500000}
-asked_h = {}
-asked_l = {}
+def format_data(account):
+    """Format the account data into printable format"""  
+    acc_name = account["name"] 
+    acc_desc = account["description"]    
+    acc_country = account["country"]
+    return (f"{acc_name}, a {acc_desc}, from {acc_country}")  
 
-for h in high:
-    if h not in asked_h:
-        asked_h[h] =  high[h]
-        print(f"Compare A: {h}")
-        break
-    break
+def check_answer(guess, follower_a, follower_b):
+    """Cgecking which account has more followers"""
+    if follower_a > follower_b:
+        return guess == "a"
+    else:
+        return guess == "b"
+score = 0
+continue_game = True
+account_b = random.choice(data)
+while continue_game:  
+    #Generate a random account from the game data
+    account_a = account_b
+    account_b = random.choice(data)
+    while account_a == account_b:
+        account_b = random.choice(data)
 
-vs_logo = '''Vs'''
-print(vs_logo)
+    print(f"Compare A: {format_data(account_a)}")
+    print(f"Compare B: {format_data(account_b)}") 
 
-for l in low:
-    if l not in asked_l:
-        asked_l[l] = low[l]
-        print(f"Against B: {l}")
-        break
-    break
+    #Ask user to guess
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-player = input("Who has more followers? Type 'A' or 'B': ").upper()
+    #check if user is correct
+    ##Get follower count of each account
+    follower_a = account_a["follower_count"]
+    follower_b = account_b["follower_count"]
+    correct = check_answer(guess, follower_a, follower_b)
+
+    if correct:
+        score += 1
+        print(f"You 're right! Current Score : {score}")
+    else:
+        continue_game = False
+        print(f"Sorry that's wrong, final score: {score}")
