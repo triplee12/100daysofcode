a4b8beab4bd4f5392e01a7e969dc44a642c84fe8
triplee12 <tripleeoliver@gmail.com>
2023-01-22 14:49:42 +0100

    Refactor code

--- day_18/shapes.py
+++ day_18/shapes.py
@@ -5,48 +5,19 @@ from turtle import Screen, Turtle
 shape = Turtle()
 
 
-def shapes():
-    """Draws different shapes"""
-    # Draw triangle
-    for i in range(3):
-        shape.forward(120)
-        shape.rt(120)
+def shapes(side_size):
+    """Draws different shapes
+    Args:
+        side_size (int): size of each shape
+    """
+    size = 360 / side_size
 
-    # Draw square
-    for i in range(4):
+    for i in range(side_size):
         shape.forward(120)
-        shape.rt(90)
+        shape.rt(size)
 
-    # Draw Pentagon
-    for i in range(5):
-        shape.forward(120)
-        shape.rt(72)
+for i in range(3, 11):
+    shapes(i)
 
-    # Draw hexagon
-    for i in range(6):
-        shape.forward(120)
-        shape.rt(60)
-
-    # Draw heptagon
-    for i in range(7):
-        shape.forward(120)
-        shape.rt(51.43)
-
-    # Draw octagon
-    for i in range(8):
-        shape.forward(120)
-        shape.rt(45)
-
-    # Draw nonagon
-    for i in range(9):
-        shape.forward(120)
-        shape.rt(40)
-
-    # Draw decagon
-    for i in range(10):
-        shape.forward(120)
-        shape.rt(36)
-
-shapes()
 screen = Screen()
 screen.exitonclick()
