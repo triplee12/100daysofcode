2634e898f2a8b44744c1a9a3f0ae044126abc440
triplee12 <tripleeoliver@gmail.com>
2023-01-23 10:36:34 +0100

    Add different line colors to the shapes

--- day_18/shapes.py
+++ day_18/shapes.py
@@ -5,19 +5,24 @@ from turtle import Screen, Turtle
 shape = Turtle()
 
 
-def shapes(side_size):
+def shapes(side_size, color):
     """Draws different shapes
     Args:
         side_size (int): size of each shape
+        color (str): color of the shape line
     """
     size = 360 / side_size
 
     for i in range(side_size):
         shape.forward(120)
         shape.rt(size)
+        shape.color(color)
+
+colors = ["#0000FF", "#EEEE3B", "#45458B", "purple", "red", "green", "pink", "black"]
 
 for i in range(3, 11):
-    shapes(i)
+    color = colors[i - 3]
+    shapes(i, color)
 
 screen = Screen()
 screen.exitonclick()
