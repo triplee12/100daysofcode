07bcba0e30a7d2a54f0a414ffe952f79d75509d0
triplee12 <tripleeoliver@gmail.com>
2023-01-23 22:41:23 +0100

    Fix code

--- day_18/spirograph.py
+++ day_18/spirograph.py
@@ -6,7 +6,7 @@ import random
 spiro = Turtle()
 colors = ["#598234", "#003b46", "#07575b", "#aebd38", "#68829e", "#505160", "#a43820", "#ba5536", "#693d3d", "#46211a", "#90afc5", "#763626", "#2a3132", "#336b87", "#80bd9e", "#89da59", "#ff420E"]
 
-for i in range(30):
+for i in range(35):
     spiro.color(random.choice(colors))
     spiro.forward(10)
     spiro.rt(10)
