2fb1d35480054b6db248d26dbfbd14fe2b6be299
triplee12 <tripleeoliver@gmail.com>
2023-01-29 17:31:38 +0100

    Implement scoreboard and check for collisions with wall

--- day_21/scoreboard.py
+++ day_21/scoreboard.py
@@ -8,17 +8,21 @@ class ScoreBoard(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.penup()
         self.color("black")
         self.score = 0
         self.goto(0, 300)
         self.write(f"Score: {self.score}", align="center", font=("Arial", 12, "normal"))
         self.hideturtle()
-        self.update_score()
 
     def update_score(self):
         """Updates the player's score"""
-        self.clear()
         self.score += 1
+        self.clear()
         self.write(f"Score: {self.score}", align="center", font=("Arial", 12, "normal"))
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align="center", font=("Arial", 12, "normal"))
 
 
