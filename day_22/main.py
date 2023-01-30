89997cc68fe76001a14771e2637ef5724c0ca05c
triplee12 <tripleeoliver@gmail.com>
2023-01-30 14:12:08 +0100

    Create the screen for pong game

---
+++ day_22/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Main module of pong game"""
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.title("Advance Pong Game")
+screen.setup(width=800, height=600)
+screen.bgcolor("#000")
+
+screen.exitonclick()
