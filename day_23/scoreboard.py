230047c94f03d4c8fe4164b8184a5d91f7424b68
triplee12 <tripleeoliver@gmail.com>
2023-02-01 20:44:58 +0100

    Create score board

---
+++ day_23/scoreboard.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Scoreboard module"""
+from turtle import Turtle
+
+
+class ScoreBoard(Turtle):
+    """Dynamically updates the player's score"""
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.color("black")
+        self.score = 0
+        self.goto(0, 260)
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 12, "normal"))
+        self.hideturtle()
+
+    def update_score(self):
+        """Updates the player's score"""
+        self.score += 1
+        self.clear()
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 12, "normal"))
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align="center", font=("Arial", 12, "normal"))
+
+
