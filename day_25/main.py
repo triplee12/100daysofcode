e8b3696ee7c3d60567b94c2f3030821121fc0b09
triplee12 <tripleeoliver@gmail.com>
2023-02-03 15:51:08 +0100

    Convert monday temperature to fahrenheit

--- day_25/main.py
+++ day_25/main.py
@@ -14,4 +14,33 @@ import pandas as pd
 #    print(temperature)
 
 pd_data = pd.read_csv("weather-data.csv")
-print(pd_data["temp"])
+#print(pd_data["temp"])
+#data_to_dict = pd_data.to_dict()
+#print(data_to_dict)
+
+#total = 0
+#data_to_list = pd_data["temp"].to_list()
+
+#for i in data_to_list:
+#    total += i
+
+# Get the average temperature
+#average = total / len(data_to_list)
+
+average = pd_data["temp"].mean()
+print(average)
+
+maximum = pd_data["temp"].max()
+print(maximum)
+
+day_with_highest_temp = pd_data[pd_data.temp == pd_data.temp.max()]
+
+print(day_with_highest_temp)
+
+# Get Monday temp and convert it to fahrenheit
+monday_data = pd_data[pd_data.day == "Monday"]
+monday_temp = monday_data.temp
+
+# Convert the temp
+fahrenheit = (monday_temp * 1.8) + 32
+print(fahrenheit)
