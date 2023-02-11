8d9cb99a19ca294e8ed614eafbcd6820df906dda
triplee12 <tripleeoliver@gmail.com>
2023-02-11 02:56:48 +0100

    Handle error

--- day_26/main.py
+++ day_26/main.py
@@ -8,8 +8,15 @@ nato_data = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_data_dict = {row.letter:row.code for index, row in nato_data.iterrows()}
 
 user_input = input("Enter a word: ").upper()
-# Split user input into list of letters
-user_list = [alphabet for alphabet in user_input]
-# Use the letter to access values from nato_data_dict
-nato_code = [nato_data_dict[letter] for letter in user_list]
-print(nato_code)
+while KeyError:
+    try:
+        # Split user input into list of letters
+        user_list = [alphabet for alphabet in user_input]
+        # Use the letter to access values from nato_data_dict
+        nato_code = [nato_data_dict[letter] for letter in user_list]
+    except KeyError:
+        print("Only alphabetical words, please")
+        user_input = input("Enter a word: ").upper()
+    else:
+        print(nato_code)
+        break
