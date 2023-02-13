7c542324608f18cab71f11dfa5eda4c0dbe9d120
triplee12 <tripleeoliver@gmail.com>
2023-02-13 18:46:49 +0100

    Fix typo

--- day_32/main.py
+++ day_32/main.py
@@ -8,9 +8,7 @@ import random
 REMOVE_WORD = "[NAME]"
 REMOVE_ANG = "Angela"
 
-##################### Extra Hard Starting Project ######################
-
-# 1. Update the birthdays.csv
+# Update the birthdays.csv
 add_to_csv = {
         "name": ["Uncle"],
         "email": ["their@me.com"],
@@ -20,11 +18,10 @@ add_to_csv = {
 }
 df = pd.DataFrame(add_to_csv)
 df.to_csv("birthdays.csv", mode='a', index=False, header=False)
-
 data = pd.read_csv("birthdays.csv")
 birth_dct = data.to_dict(orient="records")
 
-# 2. Check if today matches a birthday in the birthdays.csv
+# Check if today matches a birthday in the birthdays.csv
 date = datetime.datetime.now()
 month = date.month
 day = date.day
@@ -32,7 +29,6 @@ day = date.day
 for i in range(len(birth_dct)):
     if month == int(birth_dct[i]["month"]) and day == int(birth_dct[i]["day"]):
 
-        # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
         templates = ["letter_1.txt", "letter_2.txt", "letter_3.txt"]
         to_use = random.choice(templates)
 
@@ -41,8 +37,7 @@ for i in range(len(birth_dct)):
             message = content.replace(REMOVE_WORD, birth_dct[i]["name"])
             message = message.replace(REMOVE_ANG, "Blockdev")
 
-            # 4. Send the letter generated in step 3 to that person's email address.
             with smtplib.SMTP("smtp.gmail.com") as connection:
                 connection.starttls()
                 connection.login(user="me@gmail.com", password="mypass")
-                connection.send(from_addr="me@gmail.com", to_addrs="recieve@any.com", mgs="subject: Happy Birthday Wish\n\n{message}")
+                connection.send(from_addr="me@gmail.com", to_addrs="receive@any.com", msg="subject: Happy Birthday Wish\n\n{message}")
--- day_32/monday_quote.py
+++ day_32/monday_quote.py
@@ -10,7 +10,7 @@ with open("quotes.txt", mode="r", encoding="utf-8") as data:
 date_time = datetime.datetime.now()
 weekday = date_time.weekday()
 
-reciever = "reciever@example.com"
+receiver = "receiver@example.com"
 sender = "sender@example.com"
 password = "yourpassword"
 
@@ -19,4 +19,4 @@ if weekday == 0:
         connect.starttls()
         connect.login(user=sender, password=password)
         quote = random.choice(data_list).replace('\"', "")
-        connect.sendmail(from_addr=sender, to_addrs=reciever, msg=f"subject: Morning Quote\n\n{quote}.")
+        connect.sendmail(from_addr=sender, to_addrs=receiver, msg=f"subject: Morning Quote\n\n{quote}.")
